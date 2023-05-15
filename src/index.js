47a111e17c3a97266acae422d39a01dfef232997
Leonardo Ramirez <81320974+leoramirez99@users.noreply.github.com>
2023-05-14 22:53:17 -0300

    Instalo Boostrap y cambio los estilos de los buton

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { BrowserRouter} from 'react-router-dom';
 
-
+document.body.style = 'background: #0a0a23;';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
